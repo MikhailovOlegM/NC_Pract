ba246e592457edfecb644dcffa91ddd0a8974a82
Oleg <mikhailov.oleg.m@gmail.com>
2017-09-27 23:27:32 +0300

    sort method

--- Pract1/src/com/company/Main.java
+++ Pract1/src/com/company/Main.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-
         print(args);
+        sort(args);
     }
 
     public static void print(String[] args) {
@@ -14,5 +16,24 @@ public class Main {
         }
     }
 
+    public static void sort(String[] args) {
+        int shortestStringIndex;
+
+        for (int i = 0; i < args.length - 1; i++) {
+            shortestStringIndex = i;
+            for (int j = i + 1; j < args.length; j++) {
+                if (args[j]
+                        .compareTo(args[shortestStringIndex]) < 0) {
+                    shortestStringIndex = j;
+                }
+            }
+            if (shortestStringIndex != i) {
+                String temp = args[i];
+                args[i] = args[shortestStringIndex];
+                args[shortestStringIndex] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(args));
+    }
 
 }
